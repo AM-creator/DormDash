ed3d334eec38384f33d7a3deacfe07e23dacd6f3
AM-Creator <angelayuqi@gmail.com>
2024-02-25 02:49:40 -0500

    add error handler middleware

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,16 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-export const signup = async (req, res) => {
+import { errorHandler } from "../utils/error.js";
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   //  hash the password
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
-    //  if no errors, send back a 201 status code and a message
+ //  if no errors, send back a 201 status code and a message
     res.status(201).json("User created successfully");
   } catch (error) {
-    res.status(500).json(error.message);
+    next(error);
   }
 };
